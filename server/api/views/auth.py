9a013e177b99c5255f1958f9e877567a392d56f5
Chaimaa Laghrabi <129540587+Laghrabi@users.noreply.github.com>
2025-01-05 15:45:50 +0000

    [ADD] JWT Secret, Login & Register endpoints

--- server/api/views/auth.py
+++ server/api/views/auth.py
@@ -26,3 +26,45 @@ def isAuthenticated():
     if not current_user:
         return jsonify({'error': 'Invalid token'}), 401
     return current_user
+
+@app_views.route('/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+
+    email = data.get('email')
+    password = data.get('password')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+
+    if not email or not password or not first_name or not last_name:
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    if storage.get_specific(User, 'email', email):
+        return jsonify({'error': 'User already exists'}), 400
+
+    new_user = User(**data)
+
+    storage.new(new_user)
+    storage.save()
+
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data or 'email' not in data or 'password' not in data:
+        return jsonify({'error': 'Missing email or password'}), 400
+
+    email = data['email']
+    password = data['password']
+
+    user = storage.get_specific(User, 'email', email)
+
+    if not user or user.verify_password(password, user.password) is False:
+        return jsonify({'error': 'Invalid email or password'}), 401
+
+    token = create_access_token(identity=user)
+    return jsonify({'token': token})
